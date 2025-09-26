cf041335ca82d4a55693daf3f5a77548aa753e59
Nived-c <nived-c@industryapps.net>
2025-09-27 00:27:17 +0530

    remove .env file just hardcoded the firebase credentials

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,22 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: process.env.VITE_FIREBASE_API_KEY,
+//   authDomain: `${process.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+//   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: `${process.env.VITE_FIREBASE_PROJECT_ID}.firebasestorage.app`,
+//   appId: process.env.VITE_FIREBASE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: `${process.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.VITE_FIREBASE_PROJECT_ID}.firebasestorage.app`,
-  appId: process.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAPKcXgBJLbmngYHfOymjkV-d3t1NmhRoU",
+  authDomain: "portfolio-5e3eb.firebaseapp.com",
+  projectId: "portfolio-5e3eb",
+  storageBucket: "portfolio-5e3eb.appspot.com",
+  messagingSenderId: "511561733367",
+  appId: "1:511561733367:web:6de02fa9d4201c67e02ff2"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
